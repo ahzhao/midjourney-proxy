4776dfcf6c989fb0d2d6ad9da7535a5553dbb5f5
zhao <zhao@zhaodeMacBook-Pro.local>
2024-04-30 11:09:39 +0800

    niji 6

--- src/main/java/com/github/novicezk/midjourney/service/TaskServiceImpl.java
+++ src/main/java/com/github/novicezk/midjourney/service/TaskServiceImpl.java
@@ -73,7 +73,7 @@ public class TaskServiceImpl implements TaskService {
 		if (discordInstance == null || !discordInstance.isAlive()) {
 			return SubmitResultVO.fail(ReturnCode.NOT_FOUND, "账号不可用: " + instanceId);
 		}
-		boolean isV6 = task.getProperty(Constants.TASK_PROPERTY_FINAL_PROMPT).toString().indexOf("--v 6.0")>0;
+		boolean isV6 = task.getProperty(Constants.TASK_PROPERTY_FINAL_PROMPT).toString().indexOf("--v 6.0")>0 || task.getProperty(Constants.TASK_PROPERTY_FINAL_PROMPT).toString().indexOf("--niji 6")>0;
 		log.warn("描述:PROMPT {},isV6,{}", task.getProperty(Constants.TASK_PROPERTY_FINAL_PROMPT),isV6);
 			
 		return discordInstance.submitTask(task, () -> discordInstance.variation(targetMessageId, index, targetMessageHash, messageFlags, task.getPropertyGeneric(Constants.TASK_PROPERTY_NONCE),isV6));
