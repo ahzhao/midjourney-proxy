5a30787c40e301394ce1fdc78ada1a08ecda16cc
zhao <zhao@pc>
2025-06-18 17:54:07 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/github/novicezk/midjourney/service/DiscordServiceImpl.java

--- src/main/java/com/github/novicezk/midjourney/service/DiscordServiceImpl.java
+++ src/main/java/com/github/novicezk/midjourney/service/DiscordServiceImpl.java
@@ -70,7 +70,7 @@ public class DiscordServiceImpl implements DiscordService {
 		// log.debug("isV6:{},paramsStr:{}",isV6,paramsStr);
 		paramsStr = paramsStr.replace("$upsample_x",isV6?"upsample_v6_2x_subtle":"upsample_v6r1_2x_subtle");
 		paramsStr = new JSONObject(paramsStr).put("message_flags", messageFlags).toString();
-		log.debug("sumbit,paramsStr:{}",isV6,paramsStr);
+		// log.debug("sumbit,paramsStr:{}",isV6,paramsStr);
 		return postJsonAndCheckStatus(paramsStr);
 	}
 
